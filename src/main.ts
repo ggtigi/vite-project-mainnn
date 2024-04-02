3e60f814821991c9682b56015398620404ff9a2d
ggtigi <tigi20088@gmail.com>
2024-04-02 21:12:42 +0300

    addddd

--- src/main.ts
+++ src/main.ts
@@ -538,7 +538,7 @@ function greatestCommonDivisorr(a: number, b: number): number {
     if (b === 0) {
         return a;
     } else {
-        return greatestCommonDivisor(b, a % b);
+        return greatestCommonDivisorr(b, a % b);
     }
 }
 
@@ -675,67 +675,218 @@ calculateTravelTime(myCar, 10000)
 // 5
 // Функция сокращения объекта-дроби.
 
-interface Fraction {
-    numerator: number
-    denominator: number
+// interface Fraction {
+//     numerator: number
+//     denominator: number
+// }
+
+
+// function addFractions(fraction1: Fraction, fraction2: Fraction): Fraction {
+//     const resultNumerator = fraction1.numerator * fraction2.denominator + fraction2.numerator * fraction1.denominator
+//     const resultDenominator = fraction1.denominator * fraction2.denominator
+//     return simplifyFraction({ numerator: resultNumerator, denominator: resultDenominator })
+// }
+
+
+// function subtractFractions(fraction1: Fraction, fraction2: Fraction): Fraction {
+//     const resultNumerator = fraction1.numerator * fraction2.denominator - fraction2.numerator * fraction1.denominator
+//     const resultDenominator = fraction1.denominator * fraction2.denominator
+//     return simplifyFraction({ numerator: resultNumerator, denominator: resultDenominator })
+// }
+
+
+// function multiplyFractions(fraction1: Fraction, fraction2: Fraction): Fraction {
+//     const resultNumerator = fraction1.numerator * fraction2.numerator
+//     const resultDenominator = fraction1.denominator * fraction2.denominator
+//     return simplifyFraction({ numerator: resultNumerator, denominator: resultDenominator })
+// }
+
+
+// function divideFractions(fraction1: Fraction, fraction2: Fraction): Fraction {
+//     const resultNumerator = fraction1.numerator * fraction2.denominator
+//     const resultDenominator = fraction1.denominator * fraction2.numerator
+//     return simplifyFraction({ numerator: resultNumerator, denominator: resultDenominator })
+// }
+
+
+// function simplifyFraction(fraction: Fraction): Fraction {
+//     const gcd = greatestCommonDivisor(fraction.numerator, fraction.denominator)
+//     return {
+//         numerator: fraction.numerator / gcd,
+//         denominator: fraction.denominator / gcd
+//     };
+// }
+
+
+// function greatestCommonDivisor(a: number, b: number): number {
+//     return b == 0 ? a : greatestCommonDivisor(b, a % b)
+// }
+
+
+// const fraction1: Fraction = { numerator: 1, denominator: 2 }
+// const fraction2: Fraction = { numerator: 3, denominator: 4 }
+
+
+// console.log("Сложение:", addFractions(fraction1, fraction2))
+
+// console.log("Вычитание:", subtractFractions(fraction1, fraction2))
+
+
+// console.log("Умножение:", multiplyFractions(fraction1, fraction2))
+
+
+// console.log("Деление:", divideFractions(fraction1, fraction2))
+
+
+
+// pzM2W2.1 
+// Создать массив из 10 случайных чисел и написать несколько
+// функций для работы с ним.
+// 1
+// Функция принимает массив и выводит его на экран.
+// 2
+// Функция принимает массив и выводит только четные
+// элементы.
+// 3
+// Функция принимает массив и возвращает сумму всех
+// элементов массива.
+// 4
+// Функция принимает массив и возвращает его максималь-
+// ный элемент.
+// 5
+// Функция добавления нового элемента в массив по ука-
+// занному индексу.
+// 6
+// Функция удаления элемента из массива по указанному
+// индексу.
+
+const array: number[] = [];
+
+for (let i = 0; i < 10; i++) {
+  array.push(Math.floor(Math.random() * 100));
 }
 
-
-function addFractions(fraction1: Fraction, fraction2: Fraction): Fraction {
-    const resultNumerator = fraction1.numerator * fraction2.denominator + fraction2.numerator * fraction1.denominator
-    const resultDenominator = fraction1.denominator * fraction2.denominator
-    return simplifyFraction({ numerator: resultNumerator, denominator: resultDenominator })
+function printArray(arr: number[]): void {
+  console.log(arr);
 }
 
-
-function subtractFractions(fraction1: Fraction, fraction2: Fraction): Fraction {
-    const resultNumerator = fraction1.numerator * fraction2.denominator - fraction2.numerator * fraction1.denominator
-    const resultDenominator = fraction1.denominator * fraction2.denominator
-    return simplifyFraction({ numerator: resultNumerator, denominator: resultDenominator })
+function printEvenElements(arr: number[]): void {
+  const evenElements = arr.filter(num => num % 2 == 0);
+  console.log(evenElements);
 }
 
-
-function multiplyFractions(fraction1: Fraction, fraction2: Fraction): Fraction {
-    const resultNumerator = fraction1.numerator * fraction2.numerator
-    const resultDenominator = fraction1.denominator * fraction2.denominator
-    return simplifyFraction({ numerator: resultNumerator, denominator: resultDenominator })
+function sumArray(arr: number[]): number {
+  return arr.reduce((acc, curr) => acc + curr, 0);
 }
 
-
-function divideFractions(fraction1: Fraction, fraction2: Fraction): Fraction {
-    const resultNumerator = fraction1.numerator * fraction2.denominator
-    const resultDenominator = fraction1.denominator * fraction2.numerator
-    return simplifyFraction({ numerator: resultNumerator, denominator: resultDenominator })
+function findMaxElement(arr: number[]): number {
+  return Math.max(...arr);
 }
 
-
-function simplifyFraction(fraction: Fraction): Fraction {
-    const gcd = greatestCommonDivisor(fraction.numerator, fraction.denominator)
-    return {
-        numerator: fraction.numerator / gcd,
-        denominator: fraction.denominator / gcd
-    };
+function insertElement(arr: number[], index: number, element: number): void {
+  arr.splice(index, 0, element);
 }
 
-
-function greatestCommonDivisor(a: number, b: number): number {
-    return b === 0 ? a : greatestCommonDivisor(b, a % b)
+function deleteElement(arr: number[], index: number): void {
+  arr.splice(index, 1);
 }
 
+printArray(array);
+printEvenElements(array);
+console.log(sumArray(array));
+console.log(findMaxElement(array));
 
-const fraction1: Fraction = { numerator: 1, denominator: 2 }
-const fraction2: Fraction = { numerator: 3, denominator: 4 }
+insertElement(array, 5, 50);
+console.log(array);
+
+deleteElement(array, 2);
+console.log(array);
 
 
-console.log("Сложение:", addFractions(fraction1, fraction2))
+// 1.1.  Написать функцию возвращающюю массив целых чисел от 0 до 10
 
-console.log("Вычитание:", subtractFractions(fraction1, fraction2))
+function getNumbers(): number[] {
+    let numbers: number[] = []
+    for (let i = 0; i <= 10; i++) {
+        numbers.push(i)
+    }
+    return numbers
+}
+console.log(getNumbers())
+
+// 1.2.  Добавить в функцию параметры опциональные параметры начального и конечного значения массива
+//     (если конечный элемент массива не передат, то он больше нечального на 10) . Вызвать функцию несаколько раз
+
+function getNumberss (start: number, end: number): number[] {
+    let numbers: number[] = []
+    for (let i = start; i <= end; i++) {
+        numbers.push(i)
+    }
+    return numbers
+
+}
+console.log(getNumberss(3,8))
 
 
-console.log("Умножение:", multiplyFractions(fraction1, fraction2))
+// 1.3*. Написать функцию возвращающюю массив случайных целых чисел. Функция принимает 1 параметр, количество элементов в будущем массиве
+
+function getRandomNumbers(count: number): number[] {
+    let numbers: number[] = []
+    for (let i = 0; i < count; i++) {
+        numbers.push(Math.floor(Math.random() * 100))
+    }
+    return numbers
+}
+console.log(getRandomNumbers(5))
+
+// 2.1. В файле html создать пустой div с произвольным id
 
 
-console.log("Деление:", divideFractions(fraction1, fraction2))
+// 2.2. Получить объект div'а в js при помощи метода document.getElementById(ваш id) as HTMLDivElement
+
+
+// 2.3. Вывесли в полученный div текст, "Привет, пользователь"
+
+
+
+    function greetUser (userName?: string): string {
+        if (userName) {
+            return `Привет, ${userName}` 
+        } else {
+            return 'Привет, пользователь'
+        }
+    }
+    const myDiv: HTMLDivElement = document.getElementById ('myDiv') as HTMLDivElement
+    if (myDiv) {
+        myDiv.innerText = greetUser('Vasya')
+    }
+    myDiv.style.width = '100px'
+    myDiv.style.backgroundColor = 'aqua'
+
+
+
+// 2.4. Написать функцию, которая возвращает текст "Привет, пользователь" или "Привет, <имяПользователя>" в зависимости от переданных параметров и использовать её в задании 2.3.
+function greetUserr (userName?: string): string {
+    if (userName) {
+        return `Привет, ${userName}` 
+    } else {
+        return 'Привет, пользователь'
+    }
+}
+console.log(greetUserr('Vasya'))
+
+
+// Все функции вызывать несколько раз с разными параметрами
+// 3.1 Создать функцию, принимающую массив работников, и возвращающую массив уникальных отделов (department)
+// 3.2. Написать функцию, принимающую массив работников и ключ объекта, по которому сделать сортировку массива
+// Учесть, что строковые параметры сортируются при помощи метода localeCompare, а числовые,- вычитанием
+// 3.3. Написать функцию, аналогичную описанной в задании 2.2., но сортирующую в обратном порядке
+// 3.4. Написать функцию, принимающую массив работников и имя, и возвращающую объект сотрудника или undefined
+// 3.5. Написать функцию, принимающую массив работников и название отдела, и возвращающую новый массив, содержащий только сотрудников переданного отдела
+// 3.6. Написать функцию, принимающую массив работников и возвращающую сумму зарплат. Вызвать функцию по каждому отделу и по общему массиву
+
+
+
 
 
 
